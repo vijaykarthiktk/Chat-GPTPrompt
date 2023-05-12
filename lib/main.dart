4d79bd6b45c842999b96f0b8dd89cb80033c007e
vijaykarthiktk <vijaykarthiktk@gmail.com>
2023-05-13 02:36:57 +0530

    Added Monet Theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
-import 'dart:math';
-
 import 'package:chatgptprompt/screen/mainScreen.dart';
 import 'package:chatgptprompt/themes/themeData.dart';
 import 'package:flutter/material.dart';
+import 'package:system_theme/system_theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+  SystemTheme.accentColor;
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/themes/themeData.dart
+++ lib/themes/themeData.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:system_theme/system_theme.dart';
+
+final accentColor = SystemTheme.accentColor.accent;
 
 ThemeData themeDataLight = ThemeData(
   useMaterial3: true,
+  colorSchemeSeed: accentColor,
   brightness: Brightness.light,
 );
 
 ThemeData themeDataDark = ThemeData(
-    useMaterial3: true,
-    brightness: Brightness.dark,
+  useMaterial3: true,
+  colorSchemeSeed: accentColor,
+  brightness: Brightness.dark,
 );
